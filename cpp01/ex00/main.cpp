d3f3f68d93c08626583dee12dabbeff092840e11
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2023-12-27 15:46:58 +0000

    commiting

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 15:18:31 by codespace         #+#    #+#             */
-/*   Updated: 2023/12/27 15:18:33 by codespace        ###   ########.fr       */
+/*   Updated: 2023/12/27 15:46:41 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,19 @@
 int main()
 {
     Zombie *z = newZombie("yoyoyo");
-    randomChump("brah");
+
+	randomChump("brah");
     z->announce();
     Zombie stacker = *z;
+	*z = stacker;
+	z->set_namezzz("another is now: POTATO");
+	z->announce();
     randomChump("cenas");
     stacker.announce();
     stacker.set_namezzz("changing name");
     stacker.announce();
+
+
     delete z;
 
     return (0);
