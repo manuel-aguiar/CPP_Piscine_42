d7fe9ca6bdc781858c65263edcc1a1fa80218483
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2023-12-27 16:15:21 +0000

    commiting

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -14,7 +14,7 @@ int main()
     std::string     brain = "HI THIS IS BRAIN";
     std::string*    stringPTR = &brain;
     std::string&    stringREF = brain;
-    
+
     std::cout << "Address of string object:                        " << &brain << std::endl;
     std::cout << "Address held by pointer to string:               " << stringPTR << std::endl;
     std::cout << "Address held by reference to string:             " << &stringREF << std::endl;
@@ -23,5 +23,5 @@ int main()
     std::cout << "Value at address held by pointer to string:      " << *stringPTR << std::endl;
     std::cout << "Value at address held by reference to string:    " << stringREF << std::endl;
 
-    return 0;
+    return (0);
 }
