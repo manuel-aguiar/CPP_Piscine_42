d3892f48d4825ab30aef2b0a8d1753feeaeb6dee
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2023-12-27 16:44:45 +0000

    commiting

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -10,7 +10,7 @@ int main(int ac, char **av)
         std::cout << "Bad number of inputs" << std::endl;
         return (0);
     }
-    
+
     //necessary variables
     std::string out_name = std::string(av[1]) + ".replace";
     std::string buffer;
@@ -20,17 +20,18 @@ int main(int ac, char **av)
     size_t      start;
     size_t      index;
     size_t      len;
-    
-    
-    
+
+	std::cout << "searching for: '" << str_search << "'" << std::endl;
+
     //preparing files and open error checking
     std::remove(out_name.c_str());
     std::ifstream infile(av[1]);
     std::ofstream outfile(out_name.c_str());
+
     if (!infile.is_open())
     {
         std::cerr << "Error: infile open failed" << std::endl;
-        return (1);        
+        return (1);
     }
     if (!outfile.is_open())
     {
@@ -38,8 +39,8 @@ int main(int ac, char **av)
         infile.close();
         return (1);
     }
-    
-    
+
+
     // main block
     len = str_search.size();
     start = 0;
@@ -56,7 +57,7 @@ int main(int ac, char **av)
             // moved past the buffer, ask for more characters from std::getline
             if (start >= buffer.size())
                 break ;
-            
+
             // find str_search in the buffer
             index = buffer.find(str_search, start);
             if (index != std::string::npos)
@@ -65,9 +66,9 @@ int main(int ac, char **av)
                 outfile << str_replace;
                 start += (index - start + len);
             }
-            
+
             //str_search wasn't found, but "\n" may be a part of the substring
-            //dump all except len of str_search to guarantee you don't cut a 
+            //dump all except len of str_search to guarantee you don't cut a
             //potential candidate in half
             else if (start + len < buffer.size())
             {
@@ -84,7 +85,7 @@ int main(int ac, char **av)
     // dump whatever is left on the buffer to outfile
     if (start < buffer.size())
         outfile << buffer.substr(start);
-        
+
     //clean up
     infile.close();
     outfile.close();
