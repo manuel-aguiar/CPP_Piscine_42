e8a3db8660ce689973592ae3edf142826400d32f
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2023-12-27 13:19:59 +0000

    commiting

--- cpp00/ex01/srcs/Contact.cpp
+++ cpp00/ex01/srcs/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 11:45:24 by codespace         #+#    #+#             */
-/*   Updated: 2023/12/27 13:10:53 by codespace        ###   ########.fr       */
+/*   Updated: 2023/12/27 13:15:23 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,52 +43,52 @@ Contact &Contact::operator= (const Contact& assign)
     return (*this);
 }
 
-const std::string &Contact::getFirst() const
+const std::string& Contact::getFirst() const
 {
     return (this->first);
 }
 
-const std::string &Contact::getLast() const
+const std::string& Contact::getLast() const
 {
     return (this->last);
 }
 
-const std::string &Contact::getNick() const
+const std::string& Contact::getNick() const
 {
     return (this->nick);
 }
 
-const std::string &Contact::getPhone() const
+const std::string& Contact::getPhone() const
 {
     return (this->phone);
 }
 
-const std::string &Contact::getSecret() const
+const std::string& Contact::getSecret() const
 {
     return (this->secret);
 }
 
-void Contact::setFirst(const std::string &value)
+void Contact::setFirst(const std::string& value)
 {
     this->first = value;
 }
 
-void Contact::setLast(const std::string &value)
+void Contact::setLast(const std::string& value)
 {
     this->last = value;
 }
 
-void Contact::setNick(const std::string &value)
+void Contact::setNick(const std::string& value)
 {
     this->nick = value;
 }
 
-void Contact::setPhone(const std::string &value)
+void Contact::setPhone(const std::string& value)
 {
     this->phone = value;
 }
 
-void Contact::setSecret(const std::string &value)
+void Contact::setSecret(const std::string& value)
 {
     this->secret = value;
 }
