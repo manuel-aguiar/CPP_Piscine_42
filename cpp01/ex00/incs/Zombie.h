1b5e45123cd2952b3eb9b91945e4252c6eee1623
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-01-10 15:09:50 +0000

    commiting

--- cpp01/ex00/incs/Zombie.h
+++ cpp01/ex00/incs/Zombie.h
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 16:02:27 by codespace         #+#    #+#             */
-/*   Updated: 2023/12/28 16:06:19 by codespace        ###   ########.fr       */
+/*   Updated: 2024/01/10 15:07:15 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@
 
 Zombie  *newZombie( std::string name );
 void    randomChump( std::string name );
+
 
 #endif
