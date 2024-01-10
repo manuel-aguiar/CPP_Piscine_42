ca62fd80c11238061e480fc59a048c7dac9d219e
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-01-10 18:15:11 +0000

    commiting

--- cpp04/ex02/srcs/Animal.cpp
+++ cpp04/ex02/srcs/Animal.cpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 16:38:35 by codespace         #+#    #+#             */
-/*   Updated: 2024/01/10 13:13:19 by codespace        ###   ########.fr       */
+/*   Updated: 2024/01/10 14:20:58 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,11 +36,11 @@ Animal& Animal::operator= (const Animal& assign)
 	type = assign.type;
 	return (*this) ;
 }
-/*
+
 void	Animal::makeSound(void) const
 {
 	std::cout << "\nI am an Animal, what sound do i make?\n" << std::endl;
-}*/
+}
 
 const std::string& Animal::getType(void) const
 {
