63d0d581fec08279bd5168d9c105fee50d61aa1f
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-01-10 14:15:36 +0000

    commiting

--- cpp04/ex03/srcs/Character.cpp
+++ cpp04/ex03/srcs/Character.cpp
@@ -6,7 +6,13 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 14:11:56 by codespace         #+#    #+#             */
-/*   Updated: 2024/01/10 14:11:56 by codespace        ###   ########.fr       */
+/*   Updated: 2024/01/10 14:14:23 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "Character.hpp"
+
+Character::Character(const std::string& new_name) : name(new_name)
+{
+	std::cout << "Character constructor called" << std::endl;
+}
