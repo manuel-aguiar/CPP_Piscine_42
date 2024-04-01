d53d646e75fda4ade7ae059923f48e37d8a8ef9f
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-04-01 08:38:05 +0000

    commiting

--- cpp03/ex00/srcs/ClapTrap.cpp
+++ cpp03/ex00/srcs/ClapTrap.cpp
@@ -6,18 +6,20 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 16:36:24 by codespace         #+#    #+#             */
-/*   Updated: 2024/01/09 14:59:33 by codespace        ###   ########.fr       */
+/*   Updated: 2024/04/01 08:37:00 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/ClapTrap.hpp"
 
-ClapTrap::ClapTrap() : name(START_NAME) , hitpoints(START_HP), energypoints(START_EP), attackdamage(START_ATTACK)
+ClapTrap::ClapTrap() : name(START_NAME) , hitpoints(START_HP), \
+						energypoints(START_EP), attackdamage(START_ATTACK)
 {
 	std::cout << "Default Constructor called" << std::endl;
 }
 
-ClapTrap::ClapTrap(const std::string& start_name) : name(start_name) , hitpoints(START_HP), energypoints(START_EP), attackdamage(START_ATTACK)
+ClapTrap::ClapTrap(const std::string& start_name) : name(start_name) , hitpoints(START_HP), \
+													energypoints(START_EP), attackdamage(START_ATTACK)
 {
 	std::cout << "ClapTrap Name Constructor called: " << name << std::endl;
 }
@@ -32,10 +34,7 @@ ClapTrap::ClapTrap(const ClapTrap& copy)
 	std::cout << "ClapTrap - " << name << " - copy constructor called" << std::endl;
 	if (this == &copy)
 		return ;
-	name = copy.name;
-	hitpoints = copy.hitpoints;
-	energypoints = copy.energypoints;
-	attackdamage = copy.attackdamage;
+	*this = copy;
 }
 
 ClapTrap& ClapTrap::operator= (const ClapTrap& assign)
--- cpp03/ex00/srcs/ClapTrap_functions.cpp
+++ cpp03/ex00/srcs/ClapTrap_functions.cpp
@@ -6,81 +6,81 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 16:36:20 by codespace         #+#    #+#             */
-/*   Updated: 2023/12/28 16:36:21 by codespace        ###   ########.fr       */
+/*   Updated: 2024/04/01 08:38:01 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/ClapTrap.hpp"
 
-void    ClapTrap::attack(const std::string& target)
+void	ClapTrap::attack(const std::string& target)
 {
-    std::cout << this->name << " wants to attack " << target << ", checking energy..." << std::endl;
-    if (this->hitpoints <= 0)
-    {
-	 std::cout << this->name << " is dead, can't attack" << std::endl;
-	 return ;
-    }
-    if (this->energypoints == 0)
-    {
-	 std::cout << this->name << " has no energy left, attack failed" << std::endl;
-    }
-    else
-    {
-	 this->energypoints--;
-	 std::cout << this->name << " attacks " << target << ", causing " << this->attackdamage << " points of damage!" << std::endl;
-    }
+	std::cout << this->name << " wants to attack " << target << ", checking energy..." << std::endl;
+	if (hitpoints <= 0)
+	{
+		std::cout << this->name << " is dead, can't attack" << std::endl;
+		return ;
+	}
+	if (energypoints == 0)
+	{
+		std::cout << name << " has no energy left, attack failed" << std::endl;
+	}
+	else
+	{
+		energypoints--;
+		std::cout << name << " attacks " << target << ", causing " << attackdamage << " points of damage!" << std::endl;
+	}
 }
 
-void    ClapTrap::takeDamage(unsigned int amount)
+void	ClapTrap::takeDamage(unsigned int amount)
 {
-    std::cout << this->name << " is taking damage, checking stats..." << std::endl;
-    if (this->hitpoints == 0)
-    {
-	 std::cout << this->name << " is already dead" << std::endl;
-	 return ;
-    }
-    if (this->energypoints == 0)
-    {
-	 std::cout << this->name << " has no energy left, attack failed" << std::endl;
-    }
-    else
-    {
-	 this->hitpoints = (this->hitpoints - amount) * (this->hitpoints > amount);
-	 std::cout << this->name << " takes " << amount << " of damage! " << std::endl;
-    }
-    if (this->hitpoints == 0)
-    {
-	 std::cout << this->name << " is dead, no HP left" << std::endl;
-	 return ;
-    }
+	std::cout << name << " is taking damage, checking stats..." << std::endl;
+	if (hitpoints == 0)
+	{
+		std::cout << name << " is already dead" << std::endl;
+		return ;
+	}
+	if (energypoints == 0)
+	{
+		std::cout << name << " has no energy left, attack failed" << std::endl;
+	}
+	else
+	{
+		hitpoints = (hitpoints - amount) * (hitpoints > amount);
+		std::cout << name << " takes " << amount << " of damage! " << std::endl;
+	}
+	if (hitpoints == 0)
+	{
+		std::cout << name << " is dead, no HP left" << std::endl;
+		return ;
+	}
 }
 
-void    ClapTrap::beRepaired(unsigned int amount)
+void	ClapTrap::beRepaired(unsigned int amount)
 {
-    std::cout << this->name << " wants to repare, checking stats..." << std::endl;
-    if (this->hitpoints == 0)
-    {
-	 std::cout << this->name << " is already dead" << std::endl;
-	 return ;
-    }
-    if (this->energypoints == 0)
-    {
-	 std::cout << this->name << " has no energy left, repair failed" << std::endl;
-    }
-    else
-    {
-	 this->energypoints--;
-	 this->hitpoints += amount;
-	 std::cout << this->name << " repairs " << amount << " points! " << std::endl;
-    }
+	std::cout << name << " wants to repare, checking stats..." << std::endl;
+	if (hitpoints == 0)
+	{
+		std::cout << name << " is already dead" << std::endl;
+		return ;
+	}
+	if (energypoints == 0)
+	{
+		std::cout << name << " has no energy left, repair failed" << std::endl;
+	}
+	else
+	{
+		energypoints--;
+		hitpoints += amount;
+		std::cout << name << " repairs " << amount << " points! " << std::endl;
+	}
 }
 
-void    ClapTrap::my_stats( void )
+void	ClapTrap::my_stats( void )
 {
-    std::cout << std::endl;
-    std::cout << "Stats: " << this->name << std::endl;
-    std::cout << "   HP: " << this->hitpoints << std::endl;
-    std::cout << "   EP: " << this->energypoints << std::endl;
-    std::cout << "   Attack Strength: " << this->attackdamage << std::endl;
-    std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << "Stats: " << name << std::endl;
+	std::cout << "   HP: " << hitpoints << std::endl;
+	std::cout << "   EP: " << energypoints << std::endl;
+	std::cout << "   Attack Strength: " << attackdamage << std::endl;
+	std::cout << std::endl;
 }
