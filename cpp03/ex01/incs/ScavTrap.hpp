7a0f2decc7d9e7ed50401edc6bb992d52337d618
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-04-01 09:34:38 +0000

    commiting

--- cpp03/ex01/incs/ScavTrap.hpp
+++ cpp03/ex01/incs/ScavTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 16:36:12 by codespace         #+#    #+#             */
-/*   Updated: 2024/04/01 09:32:38 by codespace        ###   ########.fr       */
+/*   Updated: 2024/04/01 09:34:37 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ class ScavTrap : public ClapTrap
 
 		using	ClapTrap::operator=;
 
-		void	attack(const std::string& target);
+		void	attack(const std::string& target) override;
 		void    guardGate();
 
 	/*
