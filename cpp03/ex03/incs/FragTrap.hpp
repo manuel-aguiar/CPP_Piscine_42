98ace36d18fbebaac7de9e4a244e8fd0faa41152
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-04-02 12:55:12 +0000

    commiting

--- cpp03/ex03/incs/FragTrap.hpp
+++ cpp03/ex03/incs/FragTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 16:36:12 by codespace         #+#    #+#             */
-/*   Updated: 2024/04/02 12:53:46 by codespace        ###   ########.fr       */
+/*   Updated: 2024/04/02 12:54:56 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,6 @@ class FragTrap : virtual public ClapTrap
 		//using ClapTrap::operator=;
 
 		FragTrap& operator= (const FragTrap& assign);
-
-		virtual void    my_stats( void ) const;
 
 		void	highFivesGuys(void);
 };
--- cpp03/ex03/incs/ScavTrap.hpp
+++ cpp03/ex03/incs/ScavTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 16:36:12 by codespace         #+#    #+#             */
-/*   Updated: 2024/04/02 12:53:55 by codespace        ###   ########.fr       */
+/*   Updated: 2024/04/02 12:54:53 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,8 +39,6 @@ class ScavTrap : virtual public ClapTrap
 		//using ClapTrap::operator=;
 
 		ScavTrap& operator= (const ScavTrap& assign);
-
-		virtual void    my_stats( void ) const;
 
 		void	attack(const std::string& target);
 		void    guardGate();
