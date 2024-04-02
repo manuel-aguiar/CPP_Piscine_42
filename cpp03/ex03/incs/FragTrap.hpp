6fd8b9ad97ce7cf452dca60dddc7af60560a71bd
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-04-02 13:43:28 +0000

    commiting

--- cpp03/ex03/incs/FragTrap.hpp
+++ cpp03/ex03/incs/FragTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 16:36:12 by codespace         #+#    #+#             */
-/*   Updated: 2024/04/02 13:12:07 by codespace        ###   ########.fr       */
+/*   Updated: 2024/04/02 13:42:37 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,8 @@ class FragTrap : virtual public ClapTrap
 		~FragTrap();
 		FragTrap(const FragTrap& copy);
 
-		//using ClapTrap::operator=;
-
 		FragTrap& operator= (const FragTrap& assign);
-		
+
 		void	highFivesGuys(void);
 };
 
--- cpp03/ex03/incs/ScavTrap.hpp
+++ cpp03/ex03/incs/ScavTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 16:36:12 by codespace         #+#    #+#             */
-/*   Updated: 2024/04/02 13:13:15 by codespace        ###   ########.fr       */
+/*   Updated: 2024/04/02 13:42:34 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,6 @@ class ScavTrap : virtual public ClapTrap
 		ScavTrap(const std::string& start_name);
 		~ScavTrap();
 		ScavTrap(const ScavTrap& copy);
-
-		//using ClapTrap::operator=;
 
 		ScavTrap& operator= (const ScavTrap& assign);
 
