131152669722af9b6d9430478ee3ee0fb9398b41
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-04-02 12:30:43 +0000

    commiting

--- cpp03/ex03/incs/FragTrap.hpp
+++ cpp03/ex03/incs/FragTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 16:36:12 by codespace         #+#    #+#             */
-/*   Updated: 2024/04/02 11:35:01 by codespace        ###   ########.fr       */
+/*   Updated: 2024/04/02 12:30:36 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,9 @@ class FragTrap : virtual public ClapTrap
 
 	Virtual inheritance from ClapTrap, in case there are subclasses of FragTrap and
 	those inherit from Base class as well, only one instance of Base Class is instantiated
+
+	Both superclasses of DiamondTrap must inherit virtually, otherwise the compiler
+	will complaint about ambiguity
 
 */
 
