0d03969d38d2a9752e00e04da84b72f35db0cb4d
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-04-08 12:06:44 +0000

    commiting

--- cpp05/ex01/srcs/Form_exceptions.cpp
+++ cpp05/ex01/srcs/Form_exceptions.cpp
@@ -6,10 +6,48 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 11:57:53 by codespace         #+#    #+#             */
-/*   Updated: 2024/04/08 11:57:58 by codespace        ###   ########.fr       */
+/*   Updated: 2024/04/08 12:04:19 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Form.hpp"
 
+Form::GradeTooHighException::GradeTooHighException()
+{
+	#ifdef DEBUG_CONSTRUCTOR
+		std::cout << "Form GradeTooHighException Constructor Called" << std::endl;
+	#endif
+}
 
+Form::GradeTooHighException::~GradeTooHighException() throw()
+{
+	#ifdef DEBUG_CONSTRUCTOR
+		std::cout << "Form GradeTooHighException Destructor Called" << std::endl;
+	#endif
+}
+
+const char *Form::GradeTooHighException::what() const throw()
+{
+    return ("Grade: too high; range max-min: 1 - 150");
+}
+
+
+// Grade too low
+Form::GradeTooLowException::GradeTooLowException()
+{
+	#ifdef DEBUG_CONSTRUCTOR
+		std::cout << "Form GradeTooLowException Constructor Called" << std::endl;
+	#endif
+}
+
+Form::GradeTooLowException::~GradeTooLowException() throw()
+{
+	#ifdef DEBUG_CONSTRUCTOR
+		std::cout << "Form GradeTooLowException Destructor Called" << std::endl;
+	#endif
+}
+
+const char *Form::GradeTooLowException::what() const throw()
+{
+    return ("Grade: too low; range max-min: 1 - 150");
+}
