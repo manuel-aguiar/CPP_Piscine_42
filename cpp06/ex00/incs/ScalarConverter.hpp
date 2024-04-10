830d1f5376c649d6939585c5aa49df65d0b4f835
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-04-10 13:03:30 +0000

    commiting

---
+++ cpp06/ex00/incs/ScalarConverter.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/10 13:02:07 by codespace         #+#    #+#             */
+/*   Updated: 2024/04/10 13:02:38 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+
+# define SCALARCONVERTER_HPP
+
+class ScalarConverter
+{
+
+};
+
+
+
+#endif
