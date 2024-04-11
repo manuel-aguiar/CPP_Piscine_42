ce1100dc7adfb05fbc8fea7bf1f81891f2abfeb4
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-04-11 09:09:00 +0000

    commiting

--- cpp06/ex02/incs/Base.hpp
+++ cpp06/ex02/incs/Base.hpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 08:59:14 by codespace         #+#    #+#             */
-/*   Updated: 2024/04/11 09:03:31 by codespace        ###   ########.fr       */
+/*   Updated: 2024/04/11 09:07:47 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,10 @@ class Base
 	public:
 		virtual ~Base();
 };
+
+Base*	generate(void);
+void	identify(Base* p);
+void	identify(Base& p);
 
 #endif
 
--- cpp06/ex02/srcs/funcs.cpp
+++ cpp06/ex02/srcs/funcs.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   funcs.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/11 08:59:28 by codespace         #+#    #+#             */
-/*   Updated: 2024/04/11 09:08:20 by codespace        ###   ########.fr       */
+/*   Created: 2024/04/11 09:05:45 by codespace         #+#    #+#             */
+/*   Updated: 2024/04/11 09:08:55 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,20 @@
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
+#include <string>
+#include <iostream>
 
-int main(void)
+Base*	generate(void)
 {
 
-	return (0);
+}
+
+void	identify(Base* p)
+{
+
+}
+
+void	identify(Base& p)
+{
+
 }
