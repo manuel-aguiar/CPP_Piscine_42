7f1c6ed373e926006d9bfecfb6f953c87624b706
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2024-04-12 16:49:35 +0100

    commiting

--- cpp07/ex00/incs/whatever.hpp
+++ cpp07/ex00/incs/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: manuel <manuel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 15:46:51 by manuel            #+#    #+#             */
-/*   Updated: 2024/04/12 16:26:59 by manuel           ###   ########.fr       */
+/*   Updated: 2024/04/12 16:49:16 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 # define WHATEVER_HPP
 
-template <class T> void swap(T& a, T& b)
+template <typename T> void swap(T& a, T& b)
 {
     T temp;
 
@@ -23,12 +23,12 @@ template <class T> void swap(T& a, T& b)
     b = temp;
 }
 
-template <class T> T min(T a, T b)
+template <typename T> T min(T a, T b)
 {
     return (b > a ? a : b);
 }
 
-template <class T> T max(T a, T b)
+template <typename T> T max(T a, T b)
 {
     return (a > b ? a : b);
 }
