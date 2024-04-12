1f716caaa6ba76f76b133a620569b237c4b51899
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2024-04-12 17:08:43 +0100

    commiting

--- cpp07/ex01/incs/iter.hpp
+++ cpp07/ex01/incs/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: manuel <manuel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 16:44:11 by manuel            #+#    #+#             */
-/*   Updated: 2024/04/12 16:44:24 by manuel           ###   ########.fr       */
+/*   Updated: 2024/04/12 17:04:49 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,31 @@
 
 # define ITER_HPP
 
+#include <iostream>
+
+template <typename T> void iter(T arr[], size_t len, void(*func)(T&))
+{
+    for (size_t i = 0; i < len; i++)
+    {
+        func(arr[i]);
+    }
+}
+
+template <typename T> void print_arr(T arr[], size_t len)
+{
+    for (size_t i = 0; i < len; i++)
+        std::cout << arr[i] << "  ";
+    std::cout << std::endl;
+}
+
+template <typename T> void double_val(T& a)
+{
+    a = a * 2;
+}
+
+template <typename T> void print_cout(T& a)
+{
+    std::cout << a << std::endl;
+}
 
 #endif
--- cpp07/ex01/srcs/main.cpp
+++ cpp07/ex01/srcs/main.cpp
@@ -6,13 +6,48 @@
 /*   By: manuel <manuel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 16:44:33 by manuel            #+#    #+#             */
-/*   Updated: 2024/04/12 16:44:47 by manuel           ###   ########.fr       */
+/*   Updated: 2024/04/12 17:07:51 by manuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
+void    super_specific_std_string_cout(std::string& str)
+{
+    std::cout << str << std::endl;
+}
+
 int main(void)
 {
+    {
+        int arr[5] = {1,2,3,4,5};
+        size_t len = sizeof(arr) / sizeof(arr[0]);
+        
+        ::print_arr(arr, len);
+        ::iter(arr, len, ::double_val);
+        ::print_arr(arr, len);
+        std::cout << std::endl;
+
+        
+        ::iter(arr, len, ::double_val);
+        ::iter(arr, len, ::print_cout);
+        std::cout << std::endl;
+    }
+
+    {
+        std::string arr[5] = {"yoyoyo","yayaya","stfu bro","yo mad","tttttriparia"};
+        size_t len = sizeof(arr) / sizeof(arr[0]);
+        
+        ::iter(arr, len, ::print_cout);
+        std::cout << std::endl;
+    }
+
+    {
+        std::string arr[5] = {"yoyoyo","yayaya","stfu bro","yo mad","tttttriparia"};
+        size_t len = sizeof(arr) / sizeof(arr[0]);
+        
+        ::iter(arr, len, super_specific_std_string_cout);
+    }
+
     return (0);
 }
