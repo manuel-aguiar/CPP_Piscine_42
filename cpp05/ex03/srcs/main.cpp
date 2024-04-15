644e6e5247b736f0fa4a98f7da8e22835da69e61
Manuel Maria Do Nascimento Aguiar <mmaria-d@c2r1s4.42lisboa.com>
2024-04-15 18:51:35 +0100

    commiting

--- cpp05/ex03/srcs/main.cpp
+++ cpp05/ex03/srcs/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: mmaria-d <mmaria-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 15:29:09 by codespace         #+#    #+#             */
-/*   Updated: 2024/04/15 14:34:36 by codespace        ###   ########.fr       */
+/*   Updated: 2024/04/15 18:48:56 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,39 +19,123 @@
 
 int	main(void)
 {
-	Intern someRandomIntern;
-	AForm* rrf;
-	Bureaucrat boss("boss", 1);
+	{
+		std::cout << "\n\n\nMAIN 1\n" << std::endl; 
 
-	rrf = someRandomIntern.makeForm("tretas", "Bender");
-	if (rrf)
-		delete rrf;
-	rrf = someRandomIntern.makeForm("presidential pardon", "joaquim");
-	if (rrf)
-	{
-		std::cout << *rrf;
-		boss.executeForm(*rrf);
-		boss.signForm(*rrf);
-		boss.executeForm(*rrf);
-		delete rrf;
+		Intern someRandomIntern;
+		AForm* rrf;
+		Bureaucrat boss("boss", 1);
+
+		rrf = someRandomIntern.makeForm("tretas", "Bender");
+		if (rrf)
+			delete rrf;
+		rrf = someRandomIntern.makeForm("presidential pardon", "joaquim");
+		if (rrf)
+		{
+			std::cout << *dynamic_cast<PresidentialPardonForm *>(rrf);
+			boss.executeForm(*rrf);
+			boss.signForm(*rrf);
+			boss.executeForm(*rrf);
+			delete rrf;
+		}
+		rrf = someRandomIntern.makeForm("shrubbery creation", "joaquim");
+		if (rrf)
+		{
+			std::cout << *dynamic_cast<ShrubberyCreationForm *>(rrf);
+			boss.executeForm(*rrf);
+			boss.signForm(*rrf);
+			boss.executeForm(*rrf);
+			delete rrf;
+		}
+		rrf = someRandomIntern.makeForm("robotomy request", "joaquim");
+		if (rrf)
+		{
+			std::cout << *dynamic_cast<RobotomyRequestForm *>(rrf);
+			boss.executeForm(*rrf);
+			boss.signForm(*rrf);
+			boss.executeForm(*rrf);
+			delete rrf;
+		}		
 	}
-	rrf = someRandomIntern.makeForm("shrubbery creation", "joaquim");
-	if (rrf)
+
 	{
-		std::cout << *rrf;
-		boss.executeForm(*rrf);
-		boss.signForm(*rrf);
-		boss.executeForm(*rrf);
-		delete rrf;
+
+		std::cout << "\n\n\nMAIN 2\n" << std::endl; 
+
+		Intern someRandomIntern;
+		AForm* rrf;
+		Bureaucrat boss("boss", 70);
+
+		rrf = someRandomIntern.makeForm("tretas", "Bender");
+		if (rrf)
+			delete rrf;
+		rrf = someRandomIntern.makeForm("presidential pardon", "joaquim");
+		if (rrf)
+		{
+			std::cout << *dynamic_cast<PresidentialPardonForm *>(rrf);
+			boss.executeForm(*rrf);
+			boss.signForm(*rrf);
+			boss.executeForm(*rrf);
+			delete rrf;
+		}
+		rrf = someRandomIntern.makeForm("shrubbery creation", "joaquim");
+		if (rrf)
+		{
+			std::cout << *dynamic_cast<ShrubberyCreationForm *>(rrf);
+			boss.executeForm(*rrf);
+			boss.signForm(*rrf);
+			boss.executeForm(*rrf);
+			delete rrf;
+		}
+		rrf = someRandomIntern.makeForm("robotomy request", "joaquim");
+		if (rrf)
+		{
+			std::cout << *dynamic_cast<RobotomyRequestForm *>(rrf);
+			boss.executeForm(*rrf);
+			boss.signForm(*rrf);
+			boss.executeForm(*rrf);
+			delete rrf;
+		}		
 	}
-	rrf = someRandomIntern.makeForm("robotomy request", "joaquim");
-	if (rrf)
+
 	{
-		std::cout << *rrf;
-		boss.executeForm(*rrf);
-		boss.signForm(*rrf);
-		boss.executeForm(*rrf);
-		delete rrf;
+
+		std::cout << "\n\n\nMAIN 3\n" << std::endl; 
+
+		Intern someRandomIntern;
+		AForm* rrf;
+		Bureaucrat boss("boss", 70);
+
+		rrf = someRandomIntern.makeForm("tretas", "Bender");
+		if (rrf)
+			delete rrf;
+		rrf = someRandomIntern.makeForm("presidential pardon", "joaquim");
+		if (rrf)
+		{
+			std::cout << *dynamic_cast<PresidentialPardonForm *>(rrf);
+			boss.executeForm(*rrf);
+			boss.signForm(*rrf);
+			boss.executeForm(*rrf);
+			delete rrf;
+		}
+		rrf = someRandomIntern.makeForm("shrubbery creation", "joaquim");
+		if (rrf)
+		{
+			std::cout << *dynamic_cast<ShrubberyCreationForm *>(rrf);
+			boss.executeForm(*rrf);
+			boss.signForm(*rrf);
+			boss.executeForm(*rrf);
+			delete rrf;
+		}
+		rrf = someRandomIntern.makeForm("robotomy request", "joaquim");
+		if (rrf)
+		{
+			std::cout << *dynamic_cast<RobotomyRequestForm *>(rrf);
+			boss.executeForm(*rrf);
+			boss.signForm(*rrf);
+			boss.executeForm(*rrf);
+			delete rrf;
+		}		
 	}
 	return (0);
 }
