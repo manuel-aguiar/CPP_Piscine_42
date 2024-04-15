61258e89b32b6704dc72ce7b02b8a1d96931fccd
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-04-15 14:05:35 +0000

    commiting

--- cpp05/ex02/incs/Bureaucrat.hpp
+++ cpp05/ex02/incs/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 13:24:57 by codespace         #+#    #+#             */
-/*   Updated: 2024/04/15 13:15:39 by codespace        ###   ########.fr       */
+/*   Updated: 2024/04/15 14:02:18 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,6 +75,12 @@ std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureau);
 #endif
 
 /*
+	********************************************************
+
+	http://www.gotw.ca/publications/mill22.htm
+
+	A Pragmatic Look at Exception Specifications
+
 	So, throw() looks pretty useless in many cases.
 	https://stackoverflow.com/questions/1037575/why-arent-exceptions-in-c-checked-by-the-compiler
 
@@ -109,13 +115,67 @@ std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureau);
 	}
 
 	Throw() actually adds a bunch of code, no compiler checks....
+		This code is like a try-catch that the programmer doesn't write manually, but is still there
+	So assembly will show terminate calls for functions you pecify no throw (throw()),
+	so isntead of taking runtime checks out, you are actually adding them in
 
+	This function, in its body, doesn't throw anything, and still the compiler infuses such code...
+
+	Now, if you put a throw in a base class' pure virtual function, and specify a couple of exception specifications.
+	Then build a 100 Derived Classes that override it
+	Now remove a throw specification in the base class. BOOM
+
+	"
+		So here’s what seems to be the best advice we as a community have learned as of today:
+
+		Moral #1: Never write an exception specification.
+
+		Moral #2: Except possibly an empty one, but if I were you I’d avoid even that.
+	"
+
+	Not adding checks, results in less code.
+	If you are going to use them, use them where you have to...
+
+	Then an exception is not caught or doesn't match the throw(..) classes you specified
+	, compiler must call ::unexpected() which will probably lead to a terminate() :)
 
 
 ***********************************************************
 
-	http://www.gotw.ca/publications/mill22.htm
+	What problems does noexcept address?
 
-	A Pragmatic Look at Exception Specifications
+	noexcept may be resolved at compile time, the compiler can check whether it is possible
+	an exception to be thrown, if not resolve to a boolean and do add more code:
+
+
+	IN REALITY I JUST CHECKED, COMPILER CAN GET RID OF THE THROW
+	(If code is simple, that is)
+
+*********************************************************
+
+https://stackoverflow.com/questions/88573/should-i-use-an-exception-specifier-in-c/88905#88905
+
+"
+	Write code that returns common errors and throws on exceptional occasions.
+
+		Error e = open( "bla.txt" );
+		if( e == FileNotFound )
+			MessageUser( "File bla.txt not found" );
+		if( e == AccessDenied )
+			MessageUser( "Failed to open bla.txt, because we don't have read rights ..." );
+		if( e != Success )
+			MessageUser( "Failed due to some other error, error code = " + itoa( e ) );
+
+		try
+		{
+		std::vector<TObj> k( 1000 );
+		// ...
+		}
+		catch( const bad_alloc& b )
+		{
+		MessageUser( "out of memory, exiting process" );
+		throw;
+		}
+"
 
 */
