440a7131cfcd996909222afa6251db6f241beb21
Manuel Maria Do Nascimento Aguiar <mmaria-d@c2r4s8.42lisboa.com>
2024-04-22 18:22:02 +0100

    commiting

--- cpp07/ex02/srcs/main.cpp
+++ cpp07/ex02/srcs/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: mmaria-d <mmaria-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 14:54:06 by codespace         #+#    #+#             */
-/*   Updated: 2024/04/22 16:06:18 by codespace        ###   ########.fr       */
+/*   Updated: 2024/04/22 18:20:52 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -212,6 +212,58 @@ int main(void)
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
+	}
+
+	std::cout << "\n'negative size' array" << std::endl;
+
+	try
+	{
+		Array<int> arr (-1);
+		std::cout << "array size: " << arr.size() << " element 0 : " << arr[0] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	std::cout << "\n school main: " << std::endl;
+	{
+		Array<int> numbers(MAX_VAL);
+		int *mirror = new int[MAX_VAL];
+		srand(time(NULL));
+		for (int i = 0; i < MAX_VAL; i++) {
+			const int value = rand();
+			numbers[i] = value;
+			mirror[i] = value;
+		}
+		// SCOPE
+		{
+			Array<int> tmp = numbers;
+			Array<int> test(tmp);
+		}
+
+		for (int i = 0; i < MAX_VAL; i++) {
+			if (mirror[i] != numbers[i]) {
+			std::cerr << "didn't save the same value!!" << std::endl;
+			return 1;
+			}
+		}
+		try {
+			numbers[-2] = 0;
+		} catch (const std::exception &e) {
+			std::cerr << e.what() << '\n';
+		}
+		try {
+			numbers[MAX_VAL] = 0;
+		} catch (const std::exception &e) {
+			std::cerr << e.what() << '\n';
+		}
+
+		for (int i = 0; i < MAX_VAL; i++) {
+			numbers[i] = rand();
+		}
+		delete[] mirror;
+		return 0;
 	}
 
 	return (0);
