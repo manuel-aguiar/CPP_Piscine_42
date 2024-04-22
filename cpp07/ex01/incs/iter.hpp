9e4eaef6a13d636899dac1a356c50d3e848f8178
Manuel Maria Do Nascimento Aguiar <mmaria-d@c2r4s8.42lisboa.com>
2024-04-22 18:23:59 +0100

    commiting

--- cpp07/ex01/incs/iter.hpp
+++ cpp07/ex01/incs/iter.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: mmaria-d <mmaria-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 16:44:11 by manuel            #+#    #+#             */
-/*   Updated: 2024/04/22 15:34:14 by codespace        ###   ########.fr       */
+/*   Updated: 2024/04/22 18:23:47 by mmaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@
 
 #include <iostream>
 
-template <class T> void iter(T arr[], size_t len, void(*func)(T&))
+template <class T> 
+void iter(T arr[], size_t len, void(*func)(T&))
 {
     for (size_t i = 0; i < len; i++)
     {
@@ -24,19 +25,22 @@ template <class T> void iter(T arr[], size_t len, void(*func)(T&))
     }
 }
 
-template <class T> void print_arr(T arr[], size_t len)
+template <class T> 
+void print_arr(T arr[], size_t len)
 {
     for (size_t i = 0; i < len; i++)
         std::cout << arr[i] << "  ";
     std::cout << std::endl;
 }
 
-template <class T> void double_val(T& a)
+template <class T> 
+void double_val(T& a)
 {
     a = a * 2;
 }
 
-template <class T> void print_cout(T& a)
+template <class T> 
+void print_cout(T& a)
 {
     std::cout << a << std::endl;
 }
