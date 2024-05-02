1b9a79571597a641aa1dec69533806a1860dc329
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-05-02 12:56:47 +0000

    commiting

--- cpp09/ex02/incs/PmergeMe.hpp
+++ cpp09/ex02/incs/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 15:14:45 by codespace         #+#    #+#             */
-/*   Updated: 2024/05/02 12:52:14 by codespace        ###   ########.fr       */
+/*   Updated: 2024/05/02 12:56:39 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,11 +75,11 @@ class PmergeMe
 			template <
 				typename,
 				typename
-			> class Associative,
+			> class Sequencial,
 			class T,
 			class Alloc
 		>
-		void					dumpUnsorted(const std::vector<unsigned int>& unsorted, Associative<T, Alloc>& mine)
+		void					dumpUnsorted(const std::vector<unsigned int>& unsorted, Sequencial<T, Alloc>& mine)
 		{
 			mine.insert(_numbers.end(), unsorted.begin(), unsorted.end());
 		}
