e0873b0e786473233fe317f05062c3b410a79549
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-05-02 10:44:34 +0000

    commiting

---
+++ cpp09/ex02/srcs/globals.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/02 09:06:12 by codespace         #+#    #+#             */
+/*   Updated: 2024/05/02 10:40:41 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+template<typename T>
+std::string	getAllContainerName() {
+	return typeid(T).name();
+}
+
+const std::string	g_type_info[5] =
+{
+	getAllContainerName<std::vector<int> >(),
+	getAllContainerName<std::list<int> >(),
+	getAllContainerName<std::deque<int> >(),
+	getAllContainerName<std::multiset<int> >(),
+	"Unnallowed"
+};
+
+const std::string	g_type_name[5] =
+{
+	"std::vector",
+	"std::list",
+	"std::deque",
+	"std::multiset",
+	"Unnallowed"
+};
