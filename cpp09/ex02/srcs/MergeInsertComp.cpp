df81908f5de427ecd08781fcd056d763e01ccb3c
Manuel Aguiar <manuelmnaguiar00@gmail.com>
2024-05-03 09:36:44 +0100

    commiting

---
+++ cpp09/ex02/srcs/MergeInsertComp.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MergeInsertComp.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manuel <manuel@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/03 09:35:14 by manuel            #+#    #+#             */
+/*   Updated: 2024/05/03 09:35:39 by manuel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MergeInsertComp.hpp"
+
+int	is_only_spaces(std::string& word)
+{
+	for (std::string::iterator iter = word.begin(); iter != word.end(); ++iter)
+	{
+		if (*iter != '\n'
+		&& *iter != '\t'
+		&& *iter != '\v'
+		&& *iter != ' '
+		&& *iter != '\b')
+			return (false);
+	}
+	return (true);
+}
