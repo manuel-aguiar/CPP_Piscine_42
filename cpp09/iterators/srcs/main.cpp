5bc4e1971da65d6bd11dee068fa92ddbd958bd70
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-05-06 14:19:47 +0000

    commiting

--- cpp09/iterators/srcs/main.cpp
+++ cpp09/iterators/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 09:26:15 by codespace         #+#    #+#             */
-/*   Updated: 2024/05/06 14:17:57 by codespace        ###   ########.fr       */
+/*   Updated: 2024/05/06 14:19:13 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main(void)
     std::list<int> list;
     std::vector<int> vec;
 
-    for (int i = 1; i <= 5; ++i) {
+    for (int i = 1; i <= 6; ++i) {
         list.push_back(i);
         vec.push_back(i);
     }
@@ -29,9 +29,9 @@ int main(void)
 	GroupIterator<std::vector<int>::iterator> gvec(vec.begin(), 3);
     std::cout << "Vector iterator: " << gvec[0] << std::endl; // Should print 2
 
-	GroupIterator<std::list<int>::iterator> glist(list.begin(), 1);
-	GroupIterator<std::list<int>::iterator> gliste(list.end(), 1);
-    std::cout << "List iterator: " << std::binary_search(glist, gliste, 6) << std::endl; // Should print 2
+	GroupIterator<std::list<int>::iterator> glist(list.begin(), 2);
+	GroupIterator<std::list<int>::iterator> gliste(list.end(), 2);
+    std::cout << "List iterator: " << std::binary_search(glist, gliste, 1) << std::endl; // Should print 2
 
 
     return 0;
