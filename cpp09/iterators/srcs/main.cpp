cc3a7b083e239be97f18671b85a6e4a766e2e6bd
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-05-08 17:59:42 +0000

    commiting

--- cpp09/iterators/srcs/main.cpp
+++ cpp09/iterators/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 09:26:15 by codespace         #+#    #+#             */
-/*   Updated: 2024/05/08 17:58:13 by codespace        ###   ########.fr       */
+/*   Updated: 2024/05/08 17:59:40 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ int main(void)
     std::list<int> list;
     std::vector<int> vec;
 
-	int total = 5;
+	int total = 9;
     for (int i = 1; i <= total; ++i)
 	{
         list.push_back((total - i + 1) * (i % 2 ? -1 : 1));
